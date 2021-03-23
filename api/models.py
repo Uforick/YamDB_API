a6b53c4a017f3a1d836b518b7f5a0fc1a37b87df
lenitrop <lytaevsv@gmail.com>
2021-03-23 20:48:58 +0300

    Set null=True for some fields

--- api/models.py
+++ api/models.py
@@ -13,3 +13,39 @@ class Categories(models.Model):
 
     class Meta:
         ordering = ('id',)
+
+
+class Genres(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('id',)
+
+
+class Titles(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.DateField()
+    category = models.ForeignKey(
+        Categories,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        blank=True,
+        null=True,
+    )
+    genre = models.ForeignKey(
+        Genres,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('id',)
