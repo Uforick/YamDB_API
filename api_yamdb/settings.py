d40bfbd8e2ce2b1765fe4dafe914525b5c237fc4
Ilia_Plotnikov <mainplotnikov@gmail.com>
2021-03-24 19:08:06 +0300

    CustomUser step 1

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'users.apps.UsersConfig',
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
