796345dbc6f21f8199cbd3d79e8e425048ec6778
lenitrop <lytaevsv@gmail.com>
2021-03-25 02:49:35 +0300

    added endpoints for objects models viewsets

---
+++ api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
+
+from .views import CategoriesViewSet, GengresViewSet, TitlesViewSet
+
+router_v1 = SimpleRouter()
+
+router_v1.register('categories', CategoriesViewSet)
+router_v1.register('genres', GengresViewSet)
+router_v1.register('titles', TitlesViewSet)
+
+urlpatterns = [
+    path('v1/', include(router_v1.urls)),
+]
