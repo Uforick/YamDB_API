b1a66adb734ffc63fe228b0596074779137f4625
lenitrop <74257858+lenitrop@users.noreply.github.com>
2021-03-25 10:32:39 +0300

    Object models (#4)
    * test commit
    
    * Categories, Genres, Titles models
    
    * Set null=True for some fields
    
    * Some models fixes
    
    * Fixed models according to task
    
    * fixed imports formatting

--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 User = get_user_model()
 
@@ -7,6 +8,59 @@ User = get_user_model()
 class Categories(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('id',)
+
+
+class Genres(models.Model):
+    name = models.CharField(
+        max_length=200,
+        unique=True,
+    )
+    slug = models.SlugField(
+        max_length=30,
+        unique=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('id',)
+
+
+class Titles(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.SmallIntegerField()
+    rating = models.SmallIntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(10),
+        ],
+        blank=True,
+        null=True,
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+    )
+    category = models.ForeignKey(
+        Categories,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        blank=True,
+        null=True,
+        db_index=False,
+    )
+    genre = models.ManyToManyField(
+        Genres,
+        related_name='titles',
+        blank=True,
+    )
 
     def __str__(self):
         return self.name
