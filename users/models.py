63dca85570cfd2b36f74a60e1cb6d3536b835e74
Ilia_Plotnikov <mainplotnikov@gmail.com>
2021-03-26 12:18:38 +0300

    Send token CustomUser

--- users/models.py
+++ users/models.py
@@ -1,33 +1,16 @@
-from django.core.mail import send_mail
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-import secrets
-import string
-
-
-def generate_alphanum_crypt_string():
-    letters_and_digits = string.ascii_letters + string.digits
-    crypt_rand_string = ''.join(secrets.choice(
-        letters_and_digits) for i in range(12))
-    return(crypt_rand_string)
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, **extra_fields):
+    def create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError(_('The Email must be set'))
         email = self.normalize_email(email)
-        user = self.model(username=email, email=email, **extra_fields)
-        password = generate_alphanum_crypt_string()
-        send_mail(
-            'Код подтверждения email',
-            password,
-            'create_profile@yamdb.com',
-            [email],
-            fail_silently=False,
-        )
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
+        user.in_active = True
         user.save()
         return user
 
@@ -45,11 +28,13 @@ class CustomUserManager(BaseUserManager):
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     role = models.CharField(default='user', max_length=30)
+    email = models.EmailField(('email address'), unique=True)
+    username = models.CharField(max_length=30, null=True, blank=True)
 
-    USERNAME_FIELD = 'username'
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
 
     def __str__(self):
-        return self.username
+        return self.email
--- users/views.py
+++ users/views.py
@@ -1,7 +1,31 @@
 from . import serializers
+from django.contrib.auth.hashers import make_password
+from django.core.mail import send_mail
 from rest_framework.mixins import (CreateModelMixin, )
 from rest_framework.viewsets import GenericViewSet
+import secrets
+import string
+
+
+def generate_alphanum_crypt_string():
+    letters_and_digits = string.ascii_letters + string.digits
+    crypt_rand_string = ''.join(secrets.choice(
+        letters_and_digits) for i in range(12))
+    return(crypt_rand_string)
 
 
 class UserCreateMixin(CreateModelMixin, GenericViewSet):
     serializer_class = serializers.EmailForTokenSerialzer
+
+    def perform_create(self, serializer):
+        email = self.request.POST.get('email')
+        rand_string = generate_alphanum_crypt_string()
+        hashed_password = make_password(rand_string)
+        send_mail(
+            'Код подтверждения email',
+            rand_string,
+            'create_profile@yamdb.com',
+            [email],
+            fail_silently=False,
+        )
+        serializer.save(password=hashed_password, email=email, username=email)
