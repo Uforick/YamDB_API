01b1b51e75dd856b4e0368f95007d252b28f6149
Ilia_Plotnikov <mainplotnikov@gmail.com>
2021-03-27 12:28:23 +0300

    Add moderator permission

--- api/permissions.py
+++ api/permissions.py
@@ -25,15 +25,15 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         )
 
 
-# class IsOwnerOrReadOnly(permissions.BasePermission):
-#     def has_object_permission(self, request, view, obj):
-#         return (
-#             (obj.author == request.user)
-#             or (request.method in permissions.SAFE_METHODS)
-#             or request.user.is_staff
-#         )
-
-
-# class IsOwnerOnly(permissions.BasePermission):
-#     def has_permission(self, request, view, obj):
-#         return obj.author == request.user
+class IsOwnerOrModerOrAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view, obj):
+        if request.user.is_anonymous:
+            return request.method in permissions.SAFE_METHODS
+        return (
+            (request.user.is_staff)
+            or (request.user.is_superuser)
+            or (obj.author == request.user)
+            or (request.user.role == 'admin')
+            or (request.user.role == 'moderator')
+            or (request.method in permissions.SAFE_METHODS)
+        )
